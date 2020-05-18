bdd5de70195c31bbc57651a0f915d337a68c179b
Ivan Pizarro <pizarro_i@icloud.com>
2020-05-18 14:06:09 -0400

    Add Issue 14,12 and some audio effects

--- src/draw.c
+++ src/draw.c
@@ -1,19 +1,106 @@
 #include "draw.h"
 
 #include <raylib.h>
+#include <stdio.h>
 
+// Now doesnt draw a brown rect
 void draw_level(const level *lvl){
+    // ISSUE 14 drawing walls and floor 
+
+    // The textures, has many textures inside, so you have to define a part.
+    // Example the lvl texture has inside textures for diferent parts of the wall and floor
+    Rectangle frame_floor = { 57.0f, 57.0f, 48, 48};
+    Rectangle frame_hor_wall={ 52.0f, 0.0f, 48, 48};
+    Rectangle frame_vert_wall={ 180.0f, 257.0f, 48, 48};
+    Rectangle frame_out_wall={ 55.0f, 200.0f, 48, 48};
+
     // Draw cells, iterate, including borders outside the level
     for(int y=-1;y<=lvl->size_y;y++){
         for(int x=-1;x<=lvl->size_x;x++){
+            // get all nearby cells, center, right, left, up and down.
             char cell = level_get(lvl,x,y);
+            char cellr = level_get(lvl,x+1,y);
+            char celll = level_get(lvl,x-1,y);
+            char cellu = level_get(lvl,x,y-1);
+            char celld = level_get(lvl,x,y+1);
             // If the cell is a wall, paint it
-            if(cell=='#'){
-                DrawRectangle(TILE_SIZE*x,TILE_SIZE*y,TILE_SIZE,TILE_SIZE,BROWN);
+            if((cell=='#')&&(cellr=='#')&&(celll=='#')&&(cellu!='#')&&(celld!='#')){
+                Vector2 position = { TILE_SIZE*x,TILE_SIZE* y};
+                DrawTextureRec(lvl->texture_lvl,frame_hor_wall,position,RAYWHITE);
+            }else if((cell=='#')&&(cellu=='#')&&(celld=='#')){
+                Vector2 position = { TILE_SIZE*x,TILE_SIZE* y};
+                DrawTextureRec(lvl->texture_lvl,frame_vert_wall,position,RAYWHITE); 
+            }else if((cell=='#')&&(celld!='#')){
+                Vector2 position = { TILE_SIZE*x,TILE_SIZE* y};
+                DrawTextureRec(lvl->texture_lvl,frame_hor_wall,position,RAYWHITE);                
+            }else if((cell=='#')&&(cellu!='#')&&(celld=='#')){
+                Vector2 position = { TILE_SIZE*x,TILE_SIZE* y};
+                DrawTextureRec(lvl->texture_lvl,frame_vert_wall,position,RAYWHITE);
+            }else if((cell=='#')&&(cellu=='#')&&(celld=='#')&&(celll=='#')&&(cellr=='#')){
+                Vector2 position = { TILE_SIZE*x,TILE_SIZE* y};
+                DrawTextureRec(lvl->texture_lvl,frame_vert_wall,position,RAYWHITE);                                                         
+            }else{
+                Vector2 position = { TILE_SIZE*x,TILE_SIZE* y};
+                DrawTextureRec(lvl->texture_lvl,frame_floor,position,RAYWHITE);
+            }
+        }
+    }
+    // i choose a different texture for the wall in size_y row, if the dungeon is not a square , remove this part and the Rectangle frame_out_wall
+    for(int x=-1;x<=lvl->size_x;x++){
+            Vector2 position = { TILE_SIZE*x,lvl->size_y*TILE_SIZE};
+            DrawTextureRec(lvl->texture_lvl,frame_out_wall,position,RAYWHITE);
+    }
+    // ------------------------------------------------------------------------------------------------------------------
+
+}
+
+// ISSUE 14 draw dynamic objects
+
+void draw_dynamic_objects(const level *lvl, const state *sta){
+    // Torchs for hor wall
+    for(int y=-1;y<=lvl->size_y;y++){
+        for(int x=-1;x<=lvl->size_x;x+=3){
+            char cell = level_get(lvl,x,y);
+            char celld = level_get(lvl,x,y+1);
+            if((cell=='#')&&(celld!='#')){
+                Vector2 position = {TILE_SIZE*x+(TILE_SIZE-16)/2,TILE_SIZE* y+TILE_SIZE/4};
+                Rectangle frame_torch={ sta->frame*16.0f, 0.0f, 16, 26};
+                DrawTextureRec(lvl->texture_objects,frame_torch,position,RAYWHITE);   
+            }
+        }
+    } 
+    // Flags
+    for(int y=0;y<=lvl->size_y;y++){
+        for(int x=-1;x<=lvl->size_x;x+=6){
+            char cell = level_get(lvl,x,y);
+            char celld = level_get(lvl,x,y+1);
+            if((cell=='#')&&(celld!='#')){
+                Vector2 position = {TILE_SIZE*x+(TILE_SIZE-28)/2,TILE_SIZE* y+TILE_SIZE/4};
+                Rectangle frame_flag={ sta->frame*28.0f, 86.0f, 28, 26};
+                DrawTextureRec(lvl->texture_objects,frame_flag,position,RAYWHITE);   
+            }
+        }
+    } 
+    // Torchs for vertical walls 
+    for(int y=-1;y<=lvl->size_y;y+=2){
+        for(int x=-1;x<=lvl->size_x;x++){
+            char cell = level_get(lvl,x,y);
+            char cellr = level_get(lvl,x+1,y);
+            char celll = level_get(lvl,x-1,y);
+            char cellu = level_get(lvl,x,y-1);
+            char celld = level_get(lvl,x,y+1);
+            if((cell=='#')&&(cellu=='#')&&(celld=='#')&&(celll!='#')&&(cellr!='#')){
+                Vector2 position1 = {TILE_SIZE*(x+1),TILE_SIZE* y};
+                Rectangle frame_torch_right={ sta->frame*11.0f, 29.0f, 11, 24};
+                DrawTextureRec(lvl->texture_objects,frame_torch_right,position1,RAYWHITE);
+                Vector2 position2 = {TILE_SIZE*x-11,TILE_SIZE* (y)};
+                Rectangle frame_torch_left={ sta->frame*11.0f, 55.0f, 11, 24};
+                DrawTextureRec(lvl->texture_objects,frame_torch_left,position2,RAYWHITE);                   
             }
         }
     }
 }
+
 
 void draw_state(const level *lvl, const state *sta){
 
@@ -35,6 +122,7 @@ void draw_state(const level *lvl, const state *sta){
 
     // Draw level
     draw_level(lvl);
+    draw_dynamic_objects(lvl,sta); // Added to draw dynamic objects (ISSUE 14)
 
     // Draw enemies
     for(int i=0;i<sta->n_enemies;i++){
